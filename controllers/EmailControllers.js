517b5e95778820806f54b5e12dfe63d8787fbead
ghulamyaseen778 <muhammadyaseen3294@gmail.com>
2023-03-05 19:04:17 +0500

    wip

--- controllers/EmailControllers.js
+++ controllers/EmailControllers.js
@@ -1,30 +1,30 @@
 const nodemailer = require('nodemailer');
 
 const EmailControllers = {
-    postEmailController: (req, res) => {
-        console.log("let")
-        const transporter = nodemailer.createTransport({
-            service: 'ghulamyaseen67670@gmail.com',
-            auth: {
-              user: 'ghulamyaseen67670@gmail.com',
-              pass: 'bdea jvnb cfwn avqt'
-            }
-          });
-          
-          let mailOptions = {
-            from: 'ghulamyaseen67670@gmail.com',
-            to: 'anus123anus098@gmail.com',
-            subject: 'Sending Email using Node.js',
-            html: '<b>That was easy!</b>'
-          };
-          transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
-    },
+  postEmailController: (req, res) => {
+    console.log("let")
+    const transporter = nodemailer.createTransport({
+      service: 'ghulamyaseen67670@gmail.com',
+      auth: {
+        user: 'ghulamyaseen67670@gmail.com',
+        pass: 'bdea jvnb cfwn avqt'
+      }
+    });
+
+    let mailOptions = {
+      from: 'ghulamyaseen67670@gmail.com',
+      to: 'muhammadyaseen3294@gmail.com',
+      subject: 'Sending Email using Node.js',
+      html: '<b>That was easy!</b>'
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  },
 }
 
-module.exports = {EmailControllers}
+module.exports = { EmailControllers }
